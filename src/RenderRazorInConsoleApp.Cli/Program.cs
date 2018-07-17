28e76437a2e7e0554f28440ca68f6a71c412cd62
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2018-07-17 19:40:08 -0400

    isolated issue

---
+++ src/RenderRazorInConsoleApp.Cli/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.ObjectPool;
+using Microsoft.Extensions.PlatformAbstractions;
+using RenderRazorInConsoleApp.Core;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace RenderRazorInConsoleApp.Cli
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var renderer = GetRenderer();
+            var html = renderer.RenderViewToStringAsync("/View.cshtml", new Model()).GetAwaiter().GetResult();
+            System.Console.Write(html);
+            System.Console.ReadKey();
+        }
+
+        private static RazorViewToStringRenderer GetRenderer()
+        {
+            var services = new ServiceCollection();
+            var applicationEnvironment = PlatformServices.Default.Application;
+            services.AddSingleton(applicationEnvironment);
+
+            var appDirectory = Directory.GetCurrentDirectory();
+
+            var environment = new HostingEnvironment
+            {
+                ApplicationName = Assembly.GetEntryAssembly().GetName().Name
+            };
+            services.AddSingleton<IHostingEnvironment>(environment);
+
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.FileProviders.Clear();
+                options.FileProviders.Add(new PhysicalFileProvider(appDirectory));
+            });
+
+            services.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
+
+            var diagnosticSource = new DiagnosticListener("Microsoft.AspNetCore");
+            services.AddSingleton<DiagnosticSource>(diagnosticSource);
+
+            services.AddLogging();
+            services.AddMvc();
+            services.AddSingleton<RazorViewToStringRenderer>();
+            var provider = services.BuildServiceProvider();
+            return provider.GetRequiredService<RazorViewToStringRenderer>();
+        }
+    }
+}
---
+++ src/RenderRazorInConsoleApp.Core/Model.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RenderRazorInConsoleApp.Core
+{
+    class Model
+    {
+    }
+}
