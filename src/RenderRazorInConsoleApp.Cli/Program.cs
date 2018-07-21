dd7012a38125f401b8ab95970ed334f90c363f55
quinntynebrown@gmail.com <quinntynebrown@gmail.com>
2018-07-21 07:19:58 -0400

    refactor

--- src/RenderRazorInConsoleApp.Cli/Program.cs
+++ src/RenderRazorInConsoleApp.Cli/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.ObjectPool;
 using Microsoft.Extensions.PlatformAbstractions;
 using RenderRazorInConsoleApp.Core;
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
@@ -15,44 +16,70 @@ namespace RenderRazorInConsoleApp.Cli
 {
     public class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            var renderer = GetRenderer();
-            var html = await renderer.RenderViewToStringAsync("/View.cshtml", new Model());
-            System.Console.Write(html);
-            System.Console.ReadKey();
+            var serviceScopeFactory = InitializeServices();
+            var content = RenderViewAsync(serviceScopeFactory).Result;
+
+            Console.WriteLine(content);
         }
 
-        private static RazorViewToStringRenderer GetRenderer()
+        public static IServiceScopeFactory InitializeServices(string customApplicationBasePath = null)
         {
+            // Initialize the necessary services
             var services = new ServiceCollection();
-            var applicationEnvironment = PlatformServices.Default.Application;
-            services.AddSingleton(applicationEnvironment);
+            ConfigureDefaultServices(services, customApplicationBasePath);
+           
+            var serviceProvider = services.BuildServiceProvider();
+            return serviceProvider.GetRequiredService<IServiceScopeFactory>();
+        }
 
-            var appDirectory = Directory.GetCurrentDirectory();
-
-            var environment = new HostingEnvironment
+        public static Task<string> RenderViewAsync(IServiceScopeFactory scopeFactory)
+        {
+            using (var serviceScope = scopeFactory.CreateScope())
             {
-                ApplicationName = Assembly.GetEntryAssembly().GetName().Name
-            };
-            services.AddSingleton<IHostingEnvironment>(environment);
+                var helper = serviceScope.ServiceProvider.GetRequiredService<RazorViewToStringRenderer>();
 
+                var model = new RenderRazorInConsoleApp.Core.Model
+                {
+
+                };
+
+                return helper.RenderViewToStringAsync("View.cshtml", model);
+            }
+        }
+
+        private static void ConfigureDefaultServices(IServiceCollection services, string customApplicationBasePath)
+        {
+            string applicationName;
+            IFileProvider fileProvider;
+            if (!string.IsNullOrEmpty(customApplicationBasePath))
+            {
+                applicationName = Path.GetFileName(customApplicationBasePath);
+                fileProvider = new PhysicalFileProvider(customApplicationBasePath);
+            }
+            else
+            {
+                applicationName = Assembly.GetEntryAssembly().GetName().Name;
+                fileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
+            }
+
+            services.AddSingleton<IHostingEnvironment>(new HostingEnvironment
+            {
+                ApplicationName = applicationName,
+                WebRootFileProvider = fileProvider,
+            });
             services.Configure<RazorViewEngineOptions>(options =>
             {
                 options.FileProviders.Clear();
-                options.FileProviders.Add(new PhysicalFileProvider(appDirectory));
+                options.FileProviders.Add(fileProvider);
             });
-
-            services.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
-
             var diagnosticSource = new DiagnosticListener("Microsoft.AspNetCore");
+            services.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
             services.AddSingleton<DiagnosticSource>(diagnosticSource);
-
             services.AddLogging();
             services.AddMvc();
-            services.AddSingleton<RazorViewToStringRenderer>();
-            var provider = services.BuildServiceProvider();
-            return provider.GetRequiredService<RazorViewToStringRenderer>();
+            services.AddTransient<RazorViewToStringRenderer>();
         }
     }
 }
